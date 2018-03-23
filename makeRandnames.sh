b0b4a0cfe819bb3cb4dc86d845b3fc52b1fda86c
Martin Hjelmare <marhje52@kth.se>
2018-03-23 13:46:59 +0100

    Double quote to prevent globbing and word split
    * https://github.com/koalaman/shellcheck/wiki/SC2086

--- makeRandnames.sh
+++ makeRandnames.sh
@@ -5,56 +5,54 @@ OUTFOLDER=$2
 #INFOLDER='../inputfiles/'
 #OUTFOLDER='../outputfiles/'
 
-mkdir -p $OUTFOLDER
+mkdir -p "$OUTFOLDER"
 
 #This file should take converted images and give them new randome names with the date to ensure we don't randomly fail to be random.
-#It should also store a map file to get the orignal file names back. 
+#It should also store a map file to get the orignal file names back.
 
 currdate=$(date +"%Y%m%d")
-outbase=$(basename $OUTFOLDER)
+outbase=$(basename "$OUTFOLDER")
 
 #Get original file names
-#Note, if you don't have the right pattern, this code will do nothing! 
+#Note, if you don't have the right pattern, this code will do nothing!
 #Change pattern below if need be
 filepattern=$INFOLDER*_red.tif
 
 filelist=$(ls $filepattern)
 
 for file in $filelist:
-do 
-   echo $file
-   filename=$(basename $file)
+do
+   echo "$file"
+   filename=$(basename "$file")
    #echo $filename
    #strip file name to base
-   origname=$(echo $filename | awk -F'_' '{print $1"_"$2"_"$3"_"}')
+   origname=$(echo "$filename" | awk -F'_' '{print $1"_"$2"_"$3"_"}')
 
-   echo $origname
+   echo "$origname"
    #create new name
    newname=$(./genRand.sh)_$currdate
-   echo $newname
-   
-   
+   echo "$newname"
+
+
    #write out translation file
-   echo "$origname	$newname" >> ./mastertranslate_$outbase
+   echo "$origname	$newname" >> "./mastertranslate_$outbase"
 
    #move the files that match to their new place
    #mv
    #use cp for testing
    currpattern=$INFOLDER$origname
-   currfilelist=$(ls $currpattern*)
-   echo $currfilelist
+   currfilelist=$(ls "$currpattern"*)
+   echo "$currfilelist"
    for currfile in $currfilelist ;
    do
-      echo $currfile
-      currnameonly=$(basename $currfile)
-      
+      echo "$currfile"
+      currnameonly=$(basename "$currfile")
+
       currnewnameonly=${currnameonly#$origname}
       currnewname=$OUTFOLDER$newname"_"$currnewnameonly
-      echo $currnewname
-      cp $currfile $currnewname       
+      echo "$currnewname"
+      cp "$currfile" "$currnewname"
    done
 
 
 done
-
-
