25641c097346668371a011375372241a9e4e6e57
Martin Hjelmare <marhje52@kth.se>
2018-03-28 19:18:08 +0200

    Fix CLI and sort solution to upload abc

--- testchallenge/create_solution.py
+++ testchallenge/create_solution.py
@@ -12,22 +12,31 @@ def cli():
 
 
 @cli.command()
-@click.option('-s', '--solution', help='Solution file.')
-@click.option('-t', '--translation', help='Translation file.')
-@click.option('-m', '--master', help='Master output file with all info.')
-@click.option('-u', '--upload', help='File to be uploaded for scoring.')
-def create_solution(solution, translation, master_output, safe_output):
+@click.option(
+    '-s', '--solution', type=click.Path(exists=True, dir_okay=False),
+    help='Solution file.')
+@click.option(
+    '-t', '--translation', type=click.Path(exists=True, dir_okay=False),
+    help='Translation file.')
+@click.option(
+    '-m', '--master', type=click.Path(dir_okay=False, writable=True),
+    help='Master output file with all info.')
+@click.option(
+    '-u', '--upload', type=click.Path(dir_okay=False, writable=True),
+    help='File to be uploaded for scoring.')
+def create_solution(solution, translation, master, upload):
     """Create final solution csv file."""
     solution = pd.read_csv(solution)
     translation = pd.read_csv(
         translation, sep='\t', header=None, names=['basename', 'new_name'])
     basename = solution['filename'].apply(os.path.basename)
     solution = solution.assign(**{'basename': basename})
-    solution.join(translation.set_index('basename'), on='basename')
-    solution.to_csv(master_output)
+    solution = solution.join(translation.set_index('basename'), on='basename')
+    solution.to_csv(master, index=False)
     solution = solution[['new_name', 'cell_line']]
     solution = solution.rename(columns={'new_name': 'filename'})
-    solution.to_csv(safe_output)
+    solution = solution.sort_values(by=['filename'])
+    solution.to_csv(upload, index=False)
 
 
 if __name__ == '__main__':
