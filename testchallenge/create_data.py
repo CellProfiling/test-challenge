e1f46cddf80e4274cc58f76a7631552d0f863b58
Martin Hjelmare <marhje52@kth.se>
2018-03-28 15:12:59 +0200

    Add test challenge specific data creation script (#7)
    * Add filter for filename
    
    * Add test challenge specific data creation script
    
    * This can be refactored later to be more general.
    
    * Clean up
    
    * Remove index from saved csv file
    
    * Rename main function
    
    * Fix MCF7 name
    
    * Set sample size to 100 FOVs per cell line
    
    * Only allow one sample per well in total data set
    
    * Clean up
    
    * Update docstring
    
    * Harmonize sample sizes

--- testchallenge/create_data.py
+++ testchallenge/create_data.py
@@ -1,44 +1,19 @@
 """Module for data parsing."""
 import os
 
+import click
 import pandas as pd
-import numpy as np
+from sklearn.model_selection import train_test_split
 
-
-IN_ATLAS = 'In atlas 18.0'
 PLATE_ID_CUT = 300
-COLUMNS = [
-    'Antibody', 'IH state', 'Antibody state', IN_ATLAS,
-    'Ensembl id', 'Cell line', 'Plate id', 'Position',
-    'Main location', 'Other location']
 CONTAMINATED_CELLS = [
-    'U-251 MG', 'HeLa', 'PC-3', 'HEL', 'REH', 'A549', 'MCF-7',
+    'U-251 MG', 'HeLa', 'PC-3', 'HEL', 'REH', 'A549', 'MCF7',
     'U-2 OS', 'HEK 293', 'CACO-2', 'RT4']
 
-IF_IMAGE_COLUMNS = [
-    'antibody', 'Ab state', 'versions', 'ensembl_ids', 'if_plate_id',
-    'position', 'sample', 'filename', 'status', 'locations']
 
-
-def conv_list(_string):
-    """Split on comma."""
-    return _string.split(',')
-
-
-def get_lims_data(path):
-    """Return data from xls file."""
-    data = pd.read_table(
-        path, encoding='ISO-8859-1', header=0, usecols=COLUMNS,
-        dtype={
-            'Main location': str, 'Other location': str})
-    return data
-
-
-def get_if_images_data(path):
+def load_data(path):
     """Return data from if images csv."""
-    data = pd.read_csv(
-        path, header=0, usecols=IF_IMAGE_COLUMNS,
-        dtype={'versions': str, 'locations': str})
+    data = pd.read_csv(path, header=0, dtype=str)
     return data
 
 
@@ -51,62 +26,140 @@ def get_public(data, public=True):
     raise ValueError('Data is missing column versions')
 
 
-def get_filter_mask(data, values):
-    """Return boolean mask where dict values matches."""
-    return data.isin(values)
-
-
-def get_cut_mask(data, cut):
-    """Return a boolean mask of data size to cut the data in two pieces."""
-    return np.random.rand(len(data)) < cut
-
-
-def get_cut_data(data, cut):
-    """Return a two item tuple with data cut according to cut."""
-    mask = get_cut_mask(data, cut)
-    first = data[mask]
-    second = data[~mask]
-    return first, second
-
-
-def get_cell_data(data, cell_lines):
-    """Return data where cell line types match cell_lines."""
-    return data[get_filter_mask(data['Cell line'], cell_lines)]
-
-
-def get_well_paths(data):
-    """Return a Pandas series with paths to images in data."""
-    return (data['Plate id'].map(lambda x: os.path.join('/archive', str(x))) +
-            '/' + data['Plate id'].map(str) + '_' + data['Position'])
-
-
 def add_column(data, col_name, column):
     """Return data where a column has been added."""
     return data.assign(**{col_name: column})
 
 
-def pick_random_data(data, number_rows):
-    """Return randomly selected specified number of rows of data."""
-    return data.sample(number_rows)
+def include_columns(data, columns):
+    """Return data with only the specified columns."""
+    if not isinstance(columns, list):
+        columns = [columns]
+    return data[columns]
 
 
-def generate_set(lims_path, if_image_path):
-    """Generate a data set based on critera."""
-    lims_data = get_lims_data(lims_path)
-    if_image_data = get_if_images_data(if_image_path)
-    if_image_data = get_public(if_image_data)
-    # Only get data where cell line matches CONTAMINATED_CELLS.
-    lims_data = get_cell_data(lims_data, CONTAMINATED_CELLS)
-    lims_data = pick_random_data(lims_data, 2)
-    # Get if image data that is in lims data.
-    plate_filter = get_filter_mask(
-        if_image_data['if_plate_id'], lims_data['Plate id'])
-    position_filter = get_filter_mask(
-        if_image_data['position'], lims_data['Position'])
-    if_image_data = if_image_data[plate_filter]
-    if_image_data = if_image_data[position_filter]
-    return if_image_data
+def generate_set(data, exclude, include, maximum, minimum):
+    """Generate a data set based on criteria.
+
+    Args:
+        data : DataFrame to filter.
+        exclude : Dict of columns and lists of values to exclude.
+        include : Dict of columns and lists of values to include.
+        maximum : Dict of columns and values that should be max limit.
+        minimum : Dict of columns and values that should be min limit.
+
+    Returns:
+        Pandas DataFrame filtered on criteria.
+    """
+    # Exclude items.
+    for col, vals in exclude.items():
+        data = data[~data[col].isin(vals)]
+    # Include items.
+    for col, vals in include.items():
+        data = data[data[col].isin(vals)]
+    # Filter minimum.
+    for col, val in minimum.items():
+        filt = data[col].astype('float') >= val
+        data = data[filt]
+    # Filter maximum.
+    for col, val in maximum.items():
+        filt = data[col].astype('float') <= val
+        data = data[filt]
+    return data
+
+
+def generate_all(
+        path, cut, exclude=None, include=None, maximum=None, minimum=None,
+        size=None, output=None, public=None):
+    """Generate all data sets.
+
+    Args:
+        path : Path to csv file.
+        cut : Float 0..1 representing how to cut training and validation.
+        exclude : Dict of columns and lists of values to exclude.
+        include : Dict of columns and lists of values to include.
+        maximum : Dict of columns and values that should be max limit.
+        minimum : Dict of columns and values that should be min limit.
+        size : Sample size.
+        output : Path to directory where to save csv files with data.
+        public : Boolean where true means only public data should be included.
+
+    Returns:
+        Dict with two Pandas DataFrame for training and validation.
+    """
+    if include is None:
+        include = {}
+    if exclude is None:
+        exclude = {}
+    if minimum is None:
+        minimum = {}
+    if maximum is None:
+        maximum = {}
+    data = load_data(path)
+    data = data.rename(columns={'atlas_name': 'cell_line'})
+    if public is not None:
+        # Filter public/non public.
+        data = get_public(data, public=public)
+    training = []
+    validation = []
+
+    for cell_line in CONTAMINATED_CELLS:
+        include['cell_line'] = [cell_line]
+        single_set = generate_set(data, exclude, include, maximum, minimum)
+        # Add a column with well path based on column filename.
+        well_path = single_set['filename'].str.replace(r'_\d+_', '')
+        single_set = add_column(single_set, 'well_path', well_path)
+        # Drop duplicates on well path.
+        single_set = single_set.drop_duplicates('well_path')
+        # Filter randomly on sample size.
+        if size and not single_set.empty:
+            single_set = single_set.sample(size)
+        # Sets should have these columns: filename, cell_line.
+        single_set = include_columns(single_set, ['filename', 'cell_line'])
+        # Split each set 80/20 into training/validation
+        training_cut, validation_cut = train_test_split(
+            single_set, test_size=1 - cut)
+        print('Cell line: {:>8}, training: {}, validation: {}'.format(
+            cell_line, len(training_cut), len(validation_cut)))
+        training.append(training_cut)
+        validation.append(validation_cut)
+
+    # Combine all training sets and all validation sets into two sets.
+    training = pd.concat(training)
+    validation = pd.concat(validation)
+
+    if output is not None:
+        # Save both sets as two csv files.
+        for name, data in ('training', training), ('validation', validation):
+            data.to_csv('{}.csv'.format(
+                os.path.join(os.path.normpath(output), name)), index=False)
+
+    return {'training': training, 'validation': validation}
+
+
+@click.group()
+def cli():
+    """Start CLI."""
+    pass
+
+
+@cli.command()
+@click.argument('csv_file')
+@click.option('-o', '--output', help='Output to a directory')
+def create(csv_file, output=None):
+    """Run script."""
+    exclude = None  # add filelist here if needed
+    include = None
+    maximum = None
+    minimum = {'if_plate_id': PLATE_ID_CUT}
+
+    # For each cell line, make a set of sample size 200.
+    all_sets = generate_all(
+        csv_file, 0.8, exclude, include, maximum, minimum, 100, output=output,
+        public=None)
+
+    return all_sets
 
 
 if __name__ == '__main__':
-    pass
+    cli()
