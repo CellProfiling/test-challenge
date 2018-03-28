97f3f913822818d481b125bd8c62d8528364980d
Martin Hjelmare <marhje52@kth.se>
2018-03-28 18:27:51 +0200

    Add create_solution function

--- testchallenge/create_solution.py
+++ testchallenge/create_solution.py
@@ -1,8 +1,18 @@
 """Create final solution csv file."""
+import os
+
 import pandas as pd
 
 
-def create_solution(solution, renaming):
+def create_solution(sol_file, name_file, master_output, safe_output):
     """Create final solution csv file."""
-    solution = pd.read_csv(solution, header=0, dtype=str)
-    names = pd.read_csv(renaming, header=0, dtype=str)
+    solution = pd.read_csv(sol_file)
+    names = pd.read_csv(
+        name_file, sep='\t', header=None, names=['basename', 'new_name'])
+    basename = solution['filename'].apply(os.path.basename)
+    solution = solution.assign(**{'basename': basename})
+    solution.join(names.set_index('basename'), on='basename')
+    solution.to_csv(master_output)
+    solution = solution[['new_name', 'cell_line']]
+    solution = solution.rename(columns={'new_name': 'filename'})
+    solution.to_csv(safe_output)
