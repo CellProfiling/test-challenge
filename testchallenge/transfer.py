cc09007bfb1ed5556d25123ee8725d07620490dd
Martin Hjelmare <marhje52@kth.se>
2018-03-28 16:25:38 +0200

    Add exclude pattern

--- testchallenge/transfer.py
+++ testchallenge/transfer.py
@@ -1,7 +1,7 @@
 """Functions for transferring files."""
 import csv
+import re
 import shutil
-import sys
 from pathlib import Path
 
 import click
@@ -27,7 +27,7 @@ def load_csv(path):
     return files
 
 
-@cli.command(name='copy')
+@cli.command()
 @click.option(
     '-r', '--root', required=True,
     type=click.Path(exists=True, file_okay=False))
@@ -35,18 +35,20 @@ def load_csv(path):
     '-d', '--dest', required=True,
     type=click.Path(exists=True, file_okay=False, writable=True))
 @click.option('-f', '--files', required=True, type=click.Path(exists=True))
-@click.option('-p', '--pattern', required=True, type=click.STRING)
-def copy(root, dest, files, pattern):
-    """Copy files matching pattern."""
+@click.option('-e', '--exclude', required=True, type=click.STRING)
+@click.option('-i', '--include', required=True, type=click.STRING)
+def copy(root, dest, files, exclude, include):
+    """Copy files matching include pattern but not exclude pattern."""
     file_list = load_csv(files)
     for path in file_list:
         path = Path(root).joinpath(path.strip('/'))
         filename = path.name
-        glob_paths = path.parent.glob(filename + pattern)
-        path_list = list(glob_paths)
+        glob_paths = path.parent.glob(filename + include)
+        path_list = [
+            path for path in glob_paths if not re.search(exclude, path.name)]
         if not path_list:
             print('No files found')
-            sys.exit(1)
+            return
         for path_ in path_list:
             new_path = shutil.copy(path_, dest)
             print('Copied: ', path_)
